cd8ccefc7a77ccffc54d7a702f21de56851a1bdf
Koketso Baruti <18028607@vcconnect.co.za>
2025-12-18 21:33:08 +0200

    Refactor session middleware configuration for improved clarity and flexibility

--- src/app.py
+++ src/app.py
@@ -7,7 +7,14 @@ from backend.config.config import settings
 app = FastAPI()
 
 # Add session middleware
-app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
+# app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=settings.SECRET_KEY,
+    allow_origins=["http://localhost:5173", "http://localhost"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # add routes
 from backend.routes import business, login, register, booking, services
 app.include_router(login.router, prefix="/api/auth/logins")

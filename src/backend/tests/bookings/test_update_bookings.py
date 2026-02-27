54d9179ca75e946fa1bc1fb70f0af6ec672776ec
Koketso Baruti <18028607@vcconnect.co.za>
2026-02-27 15:54:14 +0200

    Revert "refactor: remove obsolete test files for bookings and availability"
    This reverts commit fef09e0ba930a25501218a1973654f29c2c4efe9.

---
+++ src/backend/tests/bookings/test_update_bookings.py
@@ -0,0 +1,4 @@
+from ...utils.database import booking_db_utils as booking_db_utils
+
+def test_update_bookings():
+    pass
--- src/backend/tests/bookings/test_validate_request.py
+++ src/backend/tests/bookings/test_validate_request.py
@@ -19,3 +19,25 @@ def test_invaild_UUID_record_value():
     with pytest.raises(ValueError, match="A valid record ID must be provided") as exec_info:
         validate_request(request=off_day_request, user_id=uuid.uuid4())
     # assert "Record ID must be provided" in str(exec_info.detail)
+import uuid
+from fastapi.testclient import TestClient
+import pytest
+from ...utils.availability_utils import validate_request
+
+user_id = uuid.UUID()
+def test_set_off_day_valid_request():
+    off_day_request = {
+    "record_id": uuid.UUID("550e8400-e29b-41d4-a716-446655440000"),
+    "request_type": "business",
+    "off_dates": ["2026-03-01", "2026-03-02"]}
+
+    actual = validate_request(request=off_day_request, user_id=user_id)
+    assert actual == True
+
+def test_invaild_UUID_value():
+    off_day_request = {"record_id": "not-a-uuid",
+                       "request_type": "OFF",
+                       "off_dates": ["2026-03-01"]}
+    with pytest.raises(ValueError) as exec_info:
+        validate_request(request=off_day_request, user_id=user_id)
+    assert "Invalid availability type requested" in str(exec_info.detail)

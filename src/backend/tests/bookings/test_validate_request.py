641c0f9f3e0cc122d266425a68420523b5faccac
Koketso Baruti <18028607@vcconnect.co.za>
2026-02-27 13:40:41 +0200

    refactor: update test cases to use SetOffDay schema and improve UUID handling

--- src/backend/tests/bookings/test_validate_request.py
+++ src/backend/tests/bookings/test_validate_request.py
@@ -2,21 +2,20 @@ import uuid
 from fastapi.testclient import TestClient
 import pytest
 from ...utils.availability_utils import validate_request
-
-user_id = uuid.UUID()
+from ...schemas.business.schedule_schema import SetOffDay
+user_id = uuid.uuid4() 
 def test_set_off_day_valid_request():
-    off_day_request = {
-    "record_id": uuid.UUID("550e8400-e29b-41d4-a716-446655440000"),
-    "request_type": "business",
-    "off_dates": ["2026-03-01", "2026-03-02"]}
+    off_day_request = SetOffDay(record_id = uuid.uuid4(),
+                                request_type = "business",
+                                off_dates = ["2026-03-01", "2026-03-02"])
 
     actual = validate_request(request=off_day_request, user_id=user_id)
     assert actual == True
 
-def test_invaild_UUID_value():
-    off_day_request = {"record_id": "not-a-uuid",
-                       "request_type": "OFF",
-                       "off_dates": ["2026-03-01"]}
+def test_invaild_UUID_record_value():
+    off_day_request = SetOffDay(record_id= "not-a-uuid",
+                       request_type = "OFF",
+                       off_dates= ["2026-03-01"])
     with pytest.raises(ValueError) as exec_info:
         validate_request(request=off_day_request, user_id=user_id)
-    assert "Invalid availability type requested" in str(exec_info.detail)
+    assert "Record ID must be provided" in str(exec_info.detail)

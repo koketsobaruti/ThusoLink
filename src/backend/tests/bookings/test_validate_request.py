b87bf99fa71ac8e340b6aa495f620b2fb3f4ed9e
Koketso Baruti <18028607@vcconnect.co.za>
2026-02-27 15:14:30 +0200

    refactor: enhance validate_request function and update test cases for SetOffDay schema

--- src/backend/tests/bookings/test_validate_request.py
+++ src/backend/tests/bookings/test_validate_request.py
@@ -1,4 +1,5 @@
 import uuid
+from fastapi import HTTPException
 from fastapi.testclient import TestClient
 import pytest
 from ...utils.availability_utils import validate_request
@@ -9,13 +10,12 @@ def test_set_off_day_valid_request():
                                 request_type = "business",
                                 off_dates = ["2026-03-01", "2026-03-02"])
 
-    actual = validate_request(request=off_day_request, user_id=user_id)
-    assert actual == True
+    validate_request(off_day_request, uuid.uuid4())
 
 def test_invaild_UUID_record_value():
-    off_day_request = SetOffDay(record_id= "not-a-uuid",
+    off_day_request = SetOffDay(record_id= "",
                        request_type = "OFF",
                        off_dates= ["2026-03-01"])
-    with pytest.raises(ValueError) as exec_info:
-        validate_request(request=off_day_request, user_id=user_id)
-    assert "Record ID must be provided" in str(exec_info.detail)
+    with pytest.raises(ValueError, match="A valid record ID must be provided") as exec_info:
+        validate_request(request=off_day_request, user_id=uuid.uuid4())
+    # assert "Record ID must be provided" in str(exec_info.detail)

27a0097fc382635063444f7bcfd2eb38a557ce41
Koketso Baruti <18028607@vcconnect.co.za>
2026-03-01 17:30:58 +0200

    Update the riased functions in wrong_service_id

--- src/backend/tests/test_business_exists.py
+++ src/backend/tests/test_business_exists.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 import pytest
 from ..database.connection import get_db
 from ..utils.database.service_db_utils import ServiceDBUtils
@@ -24,7 +26,7 @@ def test_verify_service_ownership_wrong_service_id(setup_db):
     if not setup_db:
         pytest.skip("Database connection could not be established.")
     service_db_utils = ServiceDBUtils(db = setup_db)
-    with pytest.raises(ValueError):
+    with pytest.raises(HTTPException):
         owner = service_db_utils.verify_service_ownership(service_id = "51dfeee6-c543-4ad9-92cb-b57226c99c54", 
                                                       user_id = "5650122d-e6d7-4a51-b79b-b14b804e28e6")
     assert owner == False 

2025e9bf1c55617403ef1c6374b7bf1baf450a10
Koketso Baruti <18028607@vcconnect.co.za>
2026-03-01 18:52:22 +0200

    Add aditional tests for invalid record id and invalid request type. Check validation check explicitly

--- src/backend/tests/test_off_days/test_save_off_days_unit.py
+++ src/backend/tests/test_off_days/test_save_off_days_unit.py
@@ -5,6 +5,8 @@ import pytest
 from ...database.connection import get_db
 from ...utils.database.availability_db_utils import AvailabilityDBUtils
 from ...schemas.business.schedule_schema import SetOffDay
+from pydantic import ValidationError
+
 from unittest.mock import MagicMock
 @pytest.fixture
 def mock_db():
@@ -46,7 +48,8 @@ def test_save_duplicate_days(setup_db):
                         request_type="service",
                         off_dates=["2026-03-10", "2026-03-14", "2026-03-14"])
     db_utils = AvailabilityDBUtils(db=setup_db)
-    db_utils.save_off_days(request) 
+    with pytest.raises(ValidationError):
+        db_utils.save_off_days(request) 
 
 def test_save_empty_days(setup_db):
     if not setup_db:
@@ -55,14 +58,35 @@ def test_save_empty_days(setup_db):
                         request_type="business",
                         off_dates=[])
     db_utils = AvailabilityDBUtils(db=setup_db)
-    db_utils.save_off_days(request) 
+    with pytest.raises(ValidationError):
+        db_utils.save_off_days(request) 
 
 def test_sqlalchemy_error(mock_db):
     request = SetOffDay(record_id=uuid4(),
                         request_type="business",
-                        off_dates=[])
+                        off_dates=["2026-03-14"])
     db_utils = AvailabilityDBUtils(db=mock_db)
     db_utils.save_off_days(request) 
 
     assert mock_db.execute.called
     assert mock_db.commit.called
+
+def test_invalid_request_type(setup_db):
+    if not setup_db:
+        pytest.skip("Database connection could not be established.")
+    request = SetOffDay(record_id=uuid4(),
+                        request_type="off",
+                        off_dates=["2026-04-19"])
+    db_utils = AvailabilityDBUtils(db=setup_db)
+    with pytest.raises(ValidationError):
+        db_utils.save_off_days(request) 
+
+def test_invalid_record_id(setup_db):
+    if not setup_db:
+        pytest.skip("Database connection could not be established.")
+    request = SetOffDay(record_id="not-a-uuid",
+                        request_type="off",
+                        off_dates=["2026-04-20"])
+    db_utils = AvailabilityDBUtils(db=setup_db)
+    with pytest.raises(ValidationError):
+        db_utils.save_off_days(request) 

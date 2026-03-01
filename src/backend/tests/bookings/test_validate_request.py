ed636866a74056a88283d80601fb1611d2ba70e6
Koketso Baruti <18028607@vcconnect.co.za>
2026-03-01 12:44:34 +0200

    refactor: update test_set_off_day_valid_request to use a new record_id and add additional test for invalid date type

--- src/backend/tests/bookings/test_validate_request.py
+++ src/backend/tests/bookings/test_validate_request.py
@@ -7,7 +7,7 @@ from ...schemas.business.schedule_schema import SetOffDay
 test_user_id=uuid.uuid4()
 test_record_id=uuid.uuid4()
 def test_set_off_day_valid_request():
-    off_day_request = SetOffDay(record_id = test_record_id,
+    off_day_request = SetOffDay(record_id = uuid.uuid4(),
     request_type= "business",
     off_dates = ["2026-03-01", "2026-03-02"])
     validate_request(request=off_day_request, user_id=test_user_id)
@@ -31,5 +31,12 @@ def test_invalid_request_type():
     off_day_request = SetOffDay(record_id= test_record_id,
                                 request_type= "off",
                                 off_dates = ["2026-02-29"])
+    with pytest.raises(HTTPException):
+        validate_request(request=off_day_request, user_id=test_user_id)
+
+def test_invalid_date_type():
+    off_day_request = SetOffDay(record_id= test_record_id,
+                                request_type= "business",
+                                off_dates = ["not date"])
     with pytest.raises(HTTPException):
         validate_request(request=off_day_request, user_id=test_user_id)

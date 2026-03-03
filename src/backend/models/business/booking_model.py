16536e5323508cc6536c588e68bfaa60a1f52298
Koketso Baruti <18028607@vcconnect.co.za>
2026-03-03 19:58:52 +0200

    removed additional unecessary relationships

--- src/backend/models/business/booking_model.py
+++ src/backend/models/business/booking_model.py
@@ -19,7 +19,7 @@ class ServiceBooking(Base):
     status = Column(SQLEnum(BookingStatus, name="booking_status_enum"), default=BookingStatus.REQUESTED)
 
     # booking_availability = relationship("ServiceAvailability", back_populates="bookings")  # generic relationship; for joins dynamically, use the model
-    user = relationship("User", back_populates = "service_bookings")
+    # user = relationship("User", back_populates = "service_bookings")
     
 class BusinessBooking(Base):
     __tablename__ = "business_booking"
@@ -33,7 +33,7 @@ class BusinessBooking(Base):
     status = Column(SQLEnum(BookingStatus, name="booking_status_enum"), default=BookingStatus.REQUESTED)
 
     # booking_availability = relationship("BusinessAvailability", back_populates="bookings")  # generic relationship; for joins dynamically, use the model
-    user = relationship("User", back_populates="business_bookings")
+    # user = relationship("User", back_populates="business_bookings")
 
 
 class Booking(Base):

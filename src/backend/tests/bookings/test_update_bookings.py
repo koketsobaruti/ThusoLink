51504b9c13af5d89422f1bf43288b9e3a60498a3
Koketso Baruti <18028607@vcconnect.co.za>
2026-03-19 13:24:43 +0200

    Added tests to update booking status

--- src/backend/tests/bookings/test_update_bookings.py
+++ src/backend/tests/bookings/test_update_bookings.py
@@ -7,6 +7,8 @@ from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from unittest.mock import MagicMock
 from ...schemas.business.bookings_schema import GetBooking
+from ...schemas.business.bookings_schema import UpdateBookings
+from ...schemas.business.bookings_schema import BookingStatus
 
 @pytest.fixture
 def mock_db():
@@ -23,5 +25,14 @@ def setup_db():
     except Exception as e:
         print(f"Error setting up database: {e}")
         
+def test_update_booking_status_valid():
+    # Arrange
+    booking_obj = [{"id":[uuid.UUID("fa97be97-1f81-4753-a99a-1b82477e34b4")],
+                "booking_type":"BUSINESS"}]
+    update_bookings_obj = UpdateBookings(booking_id=[uuid.UUID("fa97be97-1f81-4753-a99a-1b82477e34b4")],
+                                         status_value=BookingStatus.RESCHEDULE_REQUIRED.value)
 
+    actual_status = update_bookings_obj.status_value
+
+    assert actual_status == BookingStatus.RESCHEDULE_REQUIRED.value
 # def test_update_booking_status_valid():
